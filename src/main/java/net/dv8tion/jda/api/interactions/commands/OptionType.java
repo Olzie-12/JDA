ac0d7baf7c2ea3684eebf264c0eb22c82a1cf6da
Olzie-12 <oliver.bolter@virginmedia.com>
2022-11-12 22:57:48 +0000

    Update OptionType.java

--- src/main/java/net/dv8tion/jda/api/interactions/commands/OptionType.java
+++ src/main/java/net/dv8tion/jda/api/interactions/commands/OptionType.java
@@ -35,7 +35,7 @@ public enum OptionType
     ROLE(8),
     MENTIONABLE(9),
     NUMBER(10, true),
-    ;
+    ATTACHMENT(11);
 
     private final int raw;
     private final boolean supportsChoices;
